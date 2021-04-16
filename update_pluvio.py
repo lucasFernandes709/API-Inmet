2a3d9a2f4c93e73899323f51d023aa76127e8fad
Lucas F <lucas.sfernandes07@gmail.com>
2021-04-16 19:53:14 -0300

    Stations list loaded from file. Date range control added

--- update_pluvio.py
+++ update_pluvio.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import requests
-from datetime import datetime as dt
+from datetime import datetime, timedelta
 
 
 def print_valid_dates(formats):
@@ -19,7 +19,7 @@ def get_date(str_date: str):
 
     for format in _valid_formats_list:
         try:
-            return dt.strptime(str_date, format).date()
+            return datetime.strptime(str_date, format).date()
         except:
             pass
 
@@ -28,38 +28,52 @@ def get_date(str_date: str):
     return False
 
 
-# Base URL to access info
-URL_TEMPLATE = 'https://apitempo.inmet.gov.br/estacao/{}/{}/{}'
+def load_stations(file_path: str):
+    with open(file_path, 'r') as f:
+        station_list = f.readlines()
+
+    return list(map(lambda x: x.rstrip('\n'), station_list))
+
+
+def split_date_range(start_date, end_date, date_intervals):
+    req = []
+    start_date_int = start_date
+    for i in range(1, date_intervals + 1):
+        end_date_interval = start_date + timedelta(days=180) * i
+        req.append((str(start_date_int), str(end_date_interval), 'A240'))
+        start_date_int = end_date_interval + timedelta(days=1)
+
+    req.append((str(start_date_int), str(end_date), 'A240'))
+
+    return req
+
+
+# Constants
+URL_TEMPLATE = 'https://apitempo.inmet.gov.br/estacao/{}/{}/{}'     # Base URL to access info
+DATE_LIMIT = 180                                                    # Date range limit to 180 days
 
 # Parameter collection
 # start_date = np.datetime64('2020-01-01')
 # end_date = np.datetime64('2021-04-13')
 start_date = get_date('2020-01-01')
 end_date = get_date('2021-04-13')
+stations = load_stations('./stations.txt')
 
-stations = [('Marabá', 'A240'), ('Xinguara', 'A247')]
+# Empty DataFrame initialization
 final_df = pd.DataFrame()
 
 # Loop through stations list
-for station_name, station_cod in stations:
+for station_cod in stations:
 
-    # Requisitions list (will be passed to the API)
-    req = [(start_date, end_date, station_cod)]
-
-    # Check if dates interval is greater than 6 months
-    date_diff = (end_date - start_date).astype(int)
-    if date_diff > 180:
-        date_intervals = np.floor(date_diff / 180).astype(int)
-
-        # Generate new requisitions list, limiting date span to 180 days
-        req = []
-        start_date_int = start_date
-        for i in range(1, date_intervals + 1):
-            end_date_interval = start_date + 180 * i
-            req.append((str(start_date_int), str(end_date_interval), station_cod))
-            start_date_int = end_date_interval + 1
-
-        req.append((str(start_date_int), str(end_date), station_cod))
+    # Check if dates interval is greater than 6 months (180 days)
+    date_diff = (end_date - start_date).days
+    if date_diff > DATE_LIMIT:
+        # If true, the date range must be splitted in intervals of 180 days max
+        date_intervals = np.floor(date_diff / DATE_LIMIT).astype(int)
+        req = split_date_range(start_date, end_date, date_intervals)
+    else:
+        # Standard requisitions list (will be passed to the API)
+        req = [(str(start_date), str(end_date), station_cod)]                
 
     # Get data from Inmet API for all requisitions
     for r in req:
@@ -76,9 +90,6 @@ for station_name, station_cod in stations:
         df_agg = df_station.groupby(by='Data')['Pluviometria'].sum().round(2)
         df_agg.index = pd.to_datetime(df_agg.index, format='%Y-%m-%d').strftime('%d-%m-%Y')
         df_agg = df_agg.reset_index()
-
-        # Insert new columns
-        df_agg['Estação'] = station_name
 
         # Concat to general DataFrame
         final_df = final_df.append(df_agg, ignore_index=True)
